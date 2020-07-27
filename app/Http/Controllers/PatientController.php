ebf11a428c93d94d08b47df61cea2748cf7b745b
jasontlvg <a1247528@uabc.edu.mx>
2020-07-27 03:34:20 -0700

    API implementado y actualizado, los datos que se obtienes son variables ya

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -23,53 +23,122 @@ class PatientController extends Controller
     {
         $this->paginadoItems= 25; // 25
     }
-    public function getSectionsOld(Request $request)
+//    public function getSectionsOld(Request $request)
+//    {
+//        $decrypted = Crypt::decrypt($request->get('patient_id'));
+//
+//        // Si de plano no tiene permiso para acceder
+//        if(!Patient::find($decrypted)->survey_available){
+//            return response()->json([1,1,1], 201);
+//        }
+//
+//        // Si este paciente no tiene ningun record en la tabla, significa que no ha creado sections
+//        if(Result::where('patient_id',$decrypted)->first() == null){
+//            return response()->json([3.3,3.3], 201);
+//        }
+//
+//        $actualSurvey=Result::where('patient_id',$decrypted)->max('survey');
+//        //
+//        $countRecords= Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->get()->count();
+//        if($countRecords != 567){
+//            return response()->json([4.1,4.1], 201);
+//        }
+//        //
+//
+//        $status= [];
+//        $pagesNumber= Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->paginate($this->paginadoItems)->lastPage();
+//
+//        for ($i=1; $i<=$pagesNumber; $i++){
+//            $currentPage= $i;
+//            Paginator::currentPageResolver(function () use ($currentPage) {
+//                return $currentPage;
+//            });
+//
+//            $sizeofPaginate= sizeof(Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->paginate($this->paginadoItems)->WhereNull('answer'));
+//            if($sizeofPaginate == 0){ // Si el $sizeofPaginate es igual a 0, significa que en la seccion analizada no hay ningun null, la unica forma que pase eso, es que el paciente ya haya contestado todas las preguntas
+//                // Retornar un 1 Significa que este Section no tiene ningun null
+//                // 1 significa que todas las preguntas estan contestadas
+//                array_push($status,1);
+//            }else{
+//                // 0 significa que esta seccion tiene null
+//                array_push($status,0);
+//            }
+//        }
+//
+//        if(array_sum($status)==sizeof($status)){
+//            return response()->json([1,1,1,1], 201);
+//        }
+//
+//        return $status;
+//    }
+
+//    public function cryptProbe(Request $request)
+//    {
+//        $decrypted = Crypt::decrypt($request->get('value'));
+//        $x= $decrypted;
+//        return response()->json($x, 201);
+//    }
+
+//    public function getQuestions(Request $request)
+//    {
+//        $patient_id = Crypt::decrypt($request->get('id'));
+//        $actualSurvey=Result::where('patient_id', $patient_id)->max('survey');
+//        return Result::where('patient_id',$patient_id)->where('survey',$actualSurvey)->with('question')->paginate($this->paginadoItems);
+//        // Original
+////        $patient_id = Crypt::decrypt($request->get('id'));
+////        $actualSurvey=Result::where('patient_id', $patient_id)->max('survey');
+////        return Result::where('patient_id',$patient_id)->with('question')->paginate($this->paginadoItems);
+//    }
+
+//    public function saveAnswer(Request $request)
+//    {
+//        $patient_id = Crypt::decrypt($request->get('patient'));
+//        $checkLastSection= $request->get('checkLastSection');
+//        $completed_surveys= Crypt::decrypt($request->get('completedSurveys'));
+//        $survey_available= Crypt::decrypt($request->get('surveyAvailable'));
+//        $questionid= $request->get('id');
+//        // Ordenara todas los results en un arreglo, el primer valor del arreglo sera el record con la columna survey mas alta
+////        $result= Result::where('patient_id', $patient_id)->where('question', $request->get('id'))->where('survey',1)->orderBy('survey', 'desc')->first();
+//        $result= Result::where('patient_id', $patient_id)->where('question', $questionid)->where('survey',$completed_surveys+1)->orderBy('survey', 'desc')->first();
+////        return $result;
+//        $result->answer= $request->get('answer');
+////        if($result->save()){
+//        if($result->save()){
+//
+//            if($checkLastSection){
+//                $x=Result::where('patient_id',$patient_id)->where('survey', $completed_surveys+1)->WhereNull('answer')->get();
+//                if(count($x) == 0){
+////                    Aqui pon el codigo para actualizar los registros de encuesta contestada
+//                    $pf= Patient::find($patient_id);
+//                    $pf->survey_available=0;
+//                    $csn= $pf->completed_surveys;
+//                    $pf->completed_surveys= $csn+1;
+//                    $pf->save();
+//                }
+//            }
+//
+//            return response()->json($result, 201);
+//        }
+////        return response()->json($result, 201);
+//
+//        // Codigo este
+////        $patient_id = Crypt::decrypt($request->get('patient'));
+////        return $patient_id;
+////        $result= Result::where('patient_id', $patient_id)->update(['answer' => null]);
+//
+////        $result= Result::where('patient_id', $patient_id)->where('question', $request->get('id'))->first();
+////        $result->answer= $request->get('answer');
+////        $result->save();
+////        return response()->json($result, 201);
+//    }
+
+    public function login(Request $request)
     {
-        $decrypted = Crypt::decrypt($request->get('patient_id'));
-
-        // Si de plano no tiene permiso para acceder
-        if(!Patient::find($decrypted)->survey_available){
-            return response()->json([1,1,1], 201);
+        $patient = Patient::where('code', $request->get('code'))->first();
+        if(empty($patient)){
+            return response()->json('No existe',404);
         }
-
-        // Si este paciente no tiene ningun record en la tabla, significa que no ha creado sections
-        if(Result::where('patient_id',$decrypted)->first() == null){
-            return response()->json([3.3,3.3], 201);
-        }
-
-        $actualSurvey=Result::where('patient_id',$decrypted)->max('survey');
-        //
-        $countRecords= Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->get()->count();
-        if($countRecords != 567){
-            return response()->json([4.1,4.1], 201);
-        }
-        //
-
-        $status= [];
-        $pagesNumber= Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->paginate($this->paginadoItems)->lastPage();
-
-        for ($i=1; $i<=$pagesNumber; $i++){
-            $currentPage= $i;
-            Paginator::currentPageResolver(function () use ($currentPage) {
-                return $currentPage;
-            });
-
-            $sizeofPaginate= sizeof(Result::where('patient_id',$decrypted)->where('survey', $actualSurvey)->paginate($this->paginadoItems)->WhereNull('answer'));
-            if($sizeofPaginate == 0){ // Si el $sizeofPaginate es igual a 0, significa que en la seccion analizada no hay ningun null, la unica forma que pase eso, es que el paciente ya haya contestado todas las preguntas
-                // Retornar un 1 Significa que este Section no tiene ningun null
-                // 1 significa que todas las preguntas estan contestadas
-                array_push($status,1);
-            }else{
-                // 0 significa que esta seccion tiene null
-                array_push($status,0);
-            }
-        }
-
-        if(array_sum($status)==sizeof($status)){
-            return response()->json([1,1,1,1], 201);
-        }
-
-        return $status;
+        return response()->json($patient,201);
     }
 
     public function getSections(Request $request)
@@ -77,10 +146,15 @@ class PatientController extends Controller
 //        $userid= 27;
 //        $user = auth('patient')->user(); // Esto ya funciona, recuerda poner el token en el Header no en el body
 //        return response()->json($user,201);
+//        return response()->json(,201);
 
-        $userid= 2;
-        $completedSurveys = 1;
-        $surveyAvailable = 1;
+        $code = $request->get('code');
+
+        $patient = Patient::where('code', $code)->first();
+
+        $userid= $patient->id;
+        $completedSurveys = $patient->completed_surveys;
+        $surveyAvailable = $patient->survey_available;
         define("numberOfQuestions", 567);
 //        return numberOfQuestions;
 
@@ -166,39 +240,26 @@ class PatientController extends Controller
         return $statusSection;
     }
 
-    public function cryptProbe(Request $request)
-    {
-        $decrypted = Crypt::decrypt($request->get('value'));
-        $x= $decrypted;
-        return response()->json($x, 201);
-    }
-
-    public function getQuestions(Request $request)
-    {
-        $patient_id = Crypt::decrypt($request->get('id'));
-        $actualSurvey=Result::where('patient_id', $patient_id)->max('survey');
-        return Result::where('patient_id',$patient_id)->where('survey',$actualSurvey)->with('question')->paginate($this->paginadoItems);
-        // Original
-//        $patient_id = Crypt::decrypt($request->get('id'));
-//        $actualSurvey=Result::where('patient_id', $patient_id)->max('survey');
-//        return Result::where('patient_id',$patient_id)->with('question')->paginate($this->paginadoItems);
-    }
-
     public function getSectionData(Request $request)
     {
-
-
+//        return response()->json($request->get('code'),201);
         // Recuerda que en el link se pone ?page=2 por ejemplo y automaticamente la funcion
         // paginate() toma el valor que se envia
-        $questionsOfSurvey = Question::paginate($this->paginadoItems);
+
 //        return $questionsOfSurvey;
+        $code = $request->get('code');
+        $patient = Patient::where('code', $code)->first();
 
-//        $userid = 2;
-//        $actualSurvey = 2;
-//        $page = $request->get('page');
+        $surveyAvailable = $patient->survey_available;
 
-        $userid = 2;
-        $actualSurvey = 2;
+        if($surveyAvailable == 0){
+            return response()->json('La encuesta no esta disponible',401);
+        }
+
+        $questionsOfSurvey = Question::paginate($this->paginadoItems);
+
+        $userid = $patient->id;
+        $actualSurvey = ($patient->completed_surveys) + 1;
         $page = $request->get('page');
 
         $div = 567/$this->paginadoItems;
@@ -248,107 +309,76 @@ class PatientController extends Controller
         return response()->json($obj, 201);
     }
 
-    public function getStatusQuestionsOfSection(Request $request)
-    {
-        $userid = 27;
-        $actualSurvey = 1;
-        $page = $request->get('page');
+//    public function getStatusQuestionsOfSection(Request $request)
+//    {
+//        $userid = 27;
+//        $actualSurvey = 1;
+//        $page = $request->get('page');
+//
+//        $div = 567/$this->paginadoItems;
+//        $isInt= is_int($div);
+//        $numberOfSections = ceil($div);
+//
+//        if($numberOfSections == $page && !$isInt){
+//            $intPageQuestions = ($page-1)*$this->paginadoItems;
+//            $start = $intPageQuestions+1;
+//            $final = 567;
+//        }else{
+//            $start = ($page * $this->paginadoItems) - ($this->paginadoItems-1);
+//            $final = $page * $this->paginadoItems;
+//        }
+//        $questions = range($start, $final,1);
+//
+//        $results = Result::where('patient_id', $userid)->where('survey',$actualSurvey)->where('question', '>=', $start )->where('question', '<=', $final)->orderBy('question', 'ASC')->get();
+//        $questionsAnswered = [];
+//
+//        $final = [];
+//        foreach ($results as $result){
+//            array_push($questionsAnswered,$result->question);
+//        }
+//
+//        if($results->isEmpty()){
+//            foreach($questions as $question){
+//                $sq = (object) ['question' => $question, 'answered' => false, 'survey' => $actualSurvey];
+//                array_push($final, $sq);
+//            }
+//        }else{
+//            for($i=0; $i<sizeof($questions); $i++){
+//                if(in_array($questions[$i], $questionsAnswered)){
+//                    $sq = (object) ['question' => $questions[$i], 'answered' => true, 'survey' => $actualSurvey];
+//                }else{
+//                    $sq = (object) ['question' => $questions[$i], 'answered' => false, 'survey' => $actualSurvey];
+//                }
+//                array_push($final, $sq);
+//            }
+//        }
+//
+//        return $final;
+//    }
 
-        $div = 567/$this->paginadoItems;
-        $isInt= is_int($div);
-        $numberOfSections = ceil($div);
-
-        if($numberOfSections == $page && !$isInt){
-            $intPageQuestions = ($page-1)*$this->paginadoItems;
-            $start = $intPageQuestions+1;
-            $final = 567;
-        }else{
-            $start = ($page * $this->paginadoItems) - ($this->paginadoItems-1);
-            $final = $page * $this->paginadoItems;
-        }
-        $questions = range($start, $final,1);
-
-        $results = Result::where('patient_id', $userid)->where('survey',$actualSurvey)->where('question', '>=', $start )->where('question', '<=', $final)->orderBy('question', 'ASC')->get();
-        $questionsAnswered = [];
-
-        $final = [];
-        foreach ($results as $result){
-            array_push($questionsAnswered,$result->question);
-        }
-
-        if($results->isEmpty()){
-            foreach($questions as $question){
-                $sq = (object) ['question' => $question, 'answered' => false, 'survey' => $actualSurvey];
-                array_push($final, $sq);
-            }
-        }else{
-            for($i=0; $i<sizeof($questions); $i++){
-                if(in_array($questions[$i], $questionsAnswered)){
-                    $sq = (object) ['question' => $questions[$i], 'answered' => true, 'survey' => $actualSurvey];
-                }else{
-                    $sq = (object) ['question' => $questions[$i], 'answered' => false, 'survey' => $actualSurvey];
-                }
-                array_push($final, $sq);
-            }
-        }
-
-        return $final;
-    }
-
-    public function saveAnswer(Request $request)
-    {
-        $patient_id = Crypt::decrypt($request->get('patient'));
-        $checkLastSection= $request->get('checkLastSection');
-        $completed_surveys= Crypt::decrypt($request->get('completedSurveys'));
-        $survey_available= Crypt::decrypt($request->get('surveyAvailable'));
-        $questionid= $request->get('id');
-        // Ordenara todas los results en un arreglo, el primer valor del arreglo sera el record con la columna survey mas alta
-//        $result= Result::where('patient_id', $patient_id)->where('question', $request->get('id'))->where('survey',1)->orderBy('survey', 'desc')->first();
-        $result= Result::where('patient_id', $patient_id)->where('question', $questionid)->where('survey',$completed_surveys+1)->orderBy('survey', 'desc')->first();
-//        return $result;
-        $result->answer= $request->get('answer');
-//        if($result->save()){
-        if($result->save()){
-
-            if($checkLastSection){
-                $x=Result::where('patient_id',$patient_id)->where('survey', $completed_surveys+1)->WhereNull('answer')->get();
-                if(count($x) == 0){
-//                    Aqui pon el codigo para actualizar los registros de encuesta contestada
-                    $pf= Patient::find($patient_id);
-                    $pf->survey_available=0;
-                    $csn= $pf->completed_surveys;
-                    $pf->completed_surveys= $csn+1;
-                    $pf->save();
-                }
-            }
-
-            return response()->json($result, 201);
-        }
-//        return response()->json($result, 201);
-
-        // Codigo este
-//        $patient_id = Crypt::decrypt($request->get('patient'));
-//        return $patient_id;
-//        $result= Result::where('patient_id', $patient_id)->update(['answer' => null]);
-
-//        $result= Result::where('patient_id', $patient_id)->where('question', $request->get('id'))->first();
-//        $result->answer= $request->get('answer');
-//        $result->save();
-//        return response()->json($result, 201);
-    }
 
     public function saveAnswers(Request $request)
     {
 //        return $request->get('data');
 //        $lastSection = $request->get('lastSection') ? 'Estamos en la ultimaa seccion' : 'Aun no estamos en la ultima seccion';
-        $lastSection = $request->get('lastSection');
 
-//        return response()->json($lastSection,201);
+//        return response()->json($request->get('code'),201);
 
-        $userid = 2;
-        $survey = 2;
+        $code = $request->get('code');
+        $patient = Patient::where('code', $code)->first();
+        $surveyAvailable = $patient->survey_available;
+
+//        return response()->json('La encuesta no esta disponible',401);
+        if($surveyAvailable == 0){
+            return response()->json('La encuesta no esta disponible',401);
+        }
+
+        $userid = $patient->id;
+        $survey = ($patient->completed_surveys) + 1;
 
         $answers = $request->get('data');
+
+        $lastSection = $request->get('lastSection');
 
         $firstQuestion = $answers[0]['question'];
         $lastQuestion  = end($answers)['question'];
@@ -416,22 +446,6 @@ class PatientController extends Controller
 
 
 //        throw new \Exception('Error magnetico');
-    }
-
-    public function login(Request $request)
-    {
-        $user = Patient::where('code',$request->get('code'))->get()[0];
-        $token = auth('api')->login($user);
-        return response()->json($token,201);
-
-//        $db= Patient::where('code', $request->get('code'))->with('gender')->get();
-//        if(sizeof($db)==0){
-//            return response()->json('No hay nada', 404);
-//        }else{
-//            $db[0]->id_en= encrypt($db[0]->id);
-//            $db[0]->completed_surveys= encrypt($db[0]->completed_surveys);
-//            return response()->json($db[0], 201);
-//        }
     }
 
     public function me()
