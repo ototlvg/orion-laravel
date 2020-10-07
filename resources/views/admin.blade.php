8d5b67ac96c5dee7f9d3f91a498d25c598312044
ototlvg <ototlvg@gmail.com>
2020-10-06 22:44:01 -0700

    Data SQL añadida

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,13 +1,1 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>Orion | Admin</title><link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel=stylesheet><link href=/static/css/app.134f24064f9b87179a73db91cc3d8f4c.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.2ae2e69a05c33dfc65f8.js></script><script type=text/javascript src=/static/js/vendor.4553012ec88ed04b6100.js></script><script type=text/javascript src=/static/js/app.a3ad653b501c63fe2ccb.js></script></body></html>
-</body>
-</html>
+<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>Orion | Admin</title><link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel=stylesheet><link href=/static/css/admin.51247de4baa61c26d65d66011fb4884c.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/admin.52b0a4103ff483666323.js></script><script type=text/javascript src=/static/js/admin.4553012ec88ed04b6100.js></script><script type=text/javascript src=/static/js/admin.d0baf3892c369ecfb038.js></script></body></html>
