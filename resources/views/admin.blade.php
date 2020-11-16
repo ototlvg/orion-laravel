206e2b1748896780ca3d1fc02d99daddc2eaf291
ototlvg <ototlvg@gmail.com>
2020-11-16 14:02:28 -0800

    El URL ahora se agrega desde el blade, window.baseURL

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,1 +1,18 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>Orion | Admin</title><link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel=stylesheet><link href=/static/css/admin.da9918da39740654b09bda3a7e889534.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/admin.52b0a4103ff483666323.js></script><script type=text/javascript src=/static/js/admin.a5313c8c370071e15d4a.js></script><script type=text/javascript src=/static/js/admin.d942b284607bc91690a8.js></script></body></html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>Orion | Admin</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel=stylesheet> 
+  <link href=/static/css/admin.da9918da39740654b09bda3a7e889534.css rel=stylesheet></head>
+  <body>
+    <div id=app></div>
+
+    <script>
+        window.baseURL = 'http://mmpi.com/api/admin'
+    </script>
+    
+  <script type=text/javascript src=/static/js/admin.52b0a4103ff483666323.js></script><script type=text/javascript src=/static/js/admin.a5313c8c370071e15d4a.js></script><script type=text/javascript src=/static/js/admin.c4d4cef1ade3a70dffd1.js></script></body>
+  
+</html>
--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,1 +1,15 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>Orion</title><link href=/static/css/patient.cea44e06b412867295ac2a4066f34e66.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/patient.60198113c03343a96c96.js></script><script type=text/javascript src=/static/js/patient.d608153c9cdce62ea0c4.js></script><script type=text/javascript src=/static/js/patient.cebb6e2f3290a6571bd8.js></script></body></html>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>Orion</title>
+  <link href=/static/css/patient.32dbf83952e6754056c9b504730b2a0b.css rel=stylesheet></head>
+  <body>
+    <div id=app></div>
+
+    <script>
+        window.baseURL = 'http://mmpi.com/api'
+    </script>
+  <script type=text/javascript src=/static/js/patient.60198113c03343a96c96.js></script><script type=text/javascript src=/static/js/patient.d608153c9cdce62ea0c4.js></script><script type=text/javascript src=/static/js/patient.308265e6a4f46585d105.js></script></body>
+</html>
