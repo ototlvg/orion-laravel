af3a11d201f96c7bd85f2b756aebb8b7aa614d83
ototlvg <ototlvg@gmail.com>
2020-11-24 19:56:23 -0800

    Agregada version con nuevo logo

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html>
   <head>
     <meta charset=utf-8>
@@ -12,4 +12,20 @@
         window.baseURL = 'http://mmpi.com/api'
     </script>
   <script type=text/javascript src=/static/js/patient.60198113c03343a96c96.js></script><script type=text/javascript src=/static/js/patient.d608153c9cdce62ea0c4.js></script><script type=text/javascript src=/static/js/patient.308265e6a4f46585d105.js></script></body>
+</html> --}}
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset=utf-8>
+    <meta name=viewport content="width=device-width,initial-scale=1">
+    <title>Orion</title>
+  <link href=/static/css/patient.90f1ae82946e9283922f444723ea7430.css rel=stylesheet></head>
+  <body>
+    <div id=app></div>
+    
+    <script>
+      window.baseURL = 'http://mmpi.com/api'
+    </script>
+  <script type=text/javascript src=/static/js/patient.60198113c03343a96c96.js></script><script type=text/javascript src=/static/js/patient.d608153c9cdce62ea0c4.js></script><script type=text/javascript src=/static/js/patient.1ea54f905f541783dbb1.js></script></body>
 </html>
