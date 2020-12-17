31922d85f86cffbe8e6fad1b546bb8df2d316562
ototlvg <ototlvg@gmail.com>
2020-12-16 23:18:54 -0800

    formData la info ya se manda a llamar

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>SEPPTE | Admin</title>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel=stylesheet> 
-  <link href=/static/css/admin.da9918da39740654b09bda3a7e889534.css rel=stylesheet></head>
+    <link href=/static/css/admin.532abe5497fadea6a697fa6780255b3d.css rel=stylesheet></head>
   <body>
     <div id=app></div>
 
@@ -13,6 +13,6 @@
         window.baseURL = 'http://uabc-mmpi2.westus2.cloudapp.azure.com/api/admin'
     </script>
     
-  <script type=text/javascript src=/static/js/admin.52b0a4103ff483666323.js></script><script type=text/javascript src=/static/js/admin.a5313c8c370071e15d4a.js></script><script type=text/javascript src=/static/js/admin.c4d4cef1ade3a70dffd1.js></script></body>
+    <script type=text/javascript src=/static/js/admin.52b0a4103ff483666323.js></script><script type=text/javascript src=/static/js/admin.a5313c8c370071e15d4a.js></script><script type=text/javascript src=/static/js/admin.8a204c8f5de8ea4acfbe.js></script></body>
   
 </html>
